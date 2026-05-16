2d53d0af4de34c33d0dc48e3ebe28043aff752f8
s3lfcod3r <info@selfcoder.de>
2026-05-16 23:34:12 +0200

    a

--- src/lib/calendarCaldav.ts
+++ src/lib/calendarCaldav.ts
@@ -1,12 +1,3 @@
 export type { CaldavFetchErrorCode, CaldavWriteAction } from '@/lib/caldavSync'
-export {
-  discoverCalDavCalendars,
-  fetchCalDavOccurrencesTsdav as fetchCalDavOccurrences,
-  writeCalDavAllDayEvent,
-} from '@/lib/caldavSync'
-export type {
-  CaldavDiscoverCalendar,
-  CaldavDiscoverResult,
-  CaldavFetchResult,
-  CaldavWriteResult,
-} from '@/lib/caldavSync'
+export { discoverCalDavCalendars, writeCalDavAllDayEvent } from '@/lib/caldavSync'
+export type { CaldavDiscoverCalendar, CaldavDiscoverResult, CaldavWriteResult } from '@/lib/caldavSync'
