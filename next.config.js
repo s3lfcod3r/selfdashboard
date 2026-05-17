5afaf5f244f257e7353fe19c01434658605621a7
s3lfcod3r <info@selfcoder.de>
2026-05-17 12:53:59 +0200

    2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@
 const nextConfig = {
   output: 'standalone',
   serverExternalPackages: ['better-sqlite3', 'maxmind', 'tsdav', 'ical.js', 'rrule', 'digest-fetch'],
-  transpilePackages: ['ical.js'],
   outputFileTracingIncludes: {
     '/*': ['./node_modules/better-sqlite3/**/*'],
   },
